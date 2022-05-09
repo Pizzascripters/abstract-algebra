606603c672b08cadc26c85ea6cd4f4749e2d8ea8
Nik Olson <pizzascripters@gmail.com>
2022-05-09 11:31:15 -0500

    Create abelian group struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod group;
+
+use std::collections::HashMap;
+
+use group::Group;
+
 fn main() {
-    println!("Hello, world!");
+    let subgroups: HashMap<u32, u8> = HashMap::from([(5, 1), (4, 1)]);
+    let z20 = group::AbelianGroup::new(&subgroups);
+    println!("{}", z20.op(1, 5));
 }
