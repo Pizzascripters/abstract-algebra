74a97f064edd2215219d4260d8aec372db214d7d
Nik Olson <pizzascripters@gmail.com>
2022-05-11 13:21:00 -0500

    Rename commands module to command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod action;
-mod commands;
+mod command;
 mod group;
 mod util;
 
@@ -8,34 +8,34 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        commands::help(None);
+        command::help(None);
         return;
     };
     let primary_arg = args[1].as_str();
     match primary_arg {
         "cc" => {
             if args.len() < 3 {
-                commands::bad_usage(primary_arg.to_string())
+                command::bad_usage(primary_arg.to_string())
             } else {
-                commands::conjugacy_classes(args[2].to_string())
+                command::conjugacy_classes(args[2].to_string())
             }
         },
         "center" => {
             if args.len() < 3 {
-                commands::bad_usage(primary_arg.to_string())
+                command::bad_usage(primary_arg.to_string())
             } else {
-                commands::center(args[2].to_string())
+                command::center(args[2].to_string())
             }
         },
-        "demo" => commands::demo(),
-        "help" => commands::help(None),
+        "demo" => command::demo(),
+        "help" => command::help(None),
         "members" => {
             if args.len() < 3 {
-                commands::bad_usage(primary_arg.to_string())
+                command::bad_usage(primary_arg.to_string())
             } else {
-                commands::members(args[2].to_string())
+                command::members(args[2].to_string())
             }
         },
-        _ => commands::help(Some(primary_arg.to_string()))
+        _ => command::help(Some(primary_arg.to_string()))
     }
 }
